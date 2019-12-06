9f02448b5a8fd60d98c16aa4ec6be5b44be0de47
007javac <yurakaramyshev@yandex.com>
2019-12-06 18:49:24 +0100

    (Исправлено) 5.5. Простое число.

--- chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -6,17 +6,35 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class CheckPrimeNumberTest {
+
     @Test
-    public void when5() {
+    public void when0() {
         CheckPrimeNumber prime = new CheckPrimeNumber();
-        boolean rsl = prime.check(5);
+        boolean rsl = prime.check(0);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when1() {
+        CheckPrimeNumber prime = new CheckPrimeNumber();
+        boolean rsl = prime.check(1);
+        assertThat(rsl, is(false));
+    }
+    @Test
+    public void when3() {
+        CheckPrimeNumber prime = new CheckPrimeNumber();
+        boolean rsl = prime.check(3);
         assertThat(rsl, is(true));
     }
-
     @Test
     public void when4() {
         CheckPrimeNumber prime = new CheckPrimeNumber();
         boolean rsl = prime.check(4);
         assertThat(rsl, is(false));
+    }
+    @Test
+    public void when5() {
+        CheckPrimeNumber prime = new CheckPrimeNumber();
+        boolean rsl = prime.check(5);
+        assertThat(rsl, is(true));
     }
 }
