91e829c5a7c97615e315f4d23b87c784d1a27e9d
007javac <yurakaramyshev@yandex.com>
2020-01-06 23:52:52 +0100

    6.3. Массив заполнен true или false

---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CheckTest {
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true};
+        boolean resultArray = check.mono(input);
+        assertThat(resultArray, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true};
+        boolean resultArray = check.mono(input);
+        assertThat(resultArray, is(false));
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrueEvenSize() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true, true, true, true};
+        boolean resultArray = check.mono(input);
+        assertThat(resultArray, is(true));
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalseEvenSize() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, false, true, true, true, true};
+        boolean resultArray = check.mono(input);
+        assertThat(resultArray, is(false));
+    }
+}
