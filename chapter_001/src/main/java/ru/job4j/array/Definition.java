508a17d22055636c9ae58fe658f6d09e986527c3
007javac <yurakaramyshev@yandex.com>
2020-01-26 01:15:03 +0100

    6.0.2. Заполнение массива. [#226300]

--- chapter_001/src/main/java/ru/job4j/array/Definition.java
+++ chapter_001/src/main/java/ru/job4j/array/Definition.java
@@ -2,8 +2,14 @@ package ru.job4j.array;
 
 public class Definition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
+        //short[] ages = new short[10];
+        //String[] surnames = new String[100500];
+        //float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Миша";
+        names[1] = "Саша";
+        names[2] = "Вика";
+        names[3] = "Галя";
+        System.out.println(names[0] +" "+  names[1] +" "+ names[2] +" "+ names[3]);
     }
 }
