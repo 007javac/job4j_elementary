6cdf48c6535f644d88989419f9b66be09da1c6e5
007javac <yurakaramyshev@yandex.com>
2020-05-20 00:52:12 +0200

    1.2. Вызов метода объекта [#166085]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+/**
+ * Student.
+ * @author Karamyshev Iurii (007it@libero.it).
+ * @since 20.05.2020.
+ * @version 1
+ */
+public class Student {
+    /**
+     * два метода music и song.
+     * метода main.
+     * объект класса Student с переменной petya
+     */
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+}
