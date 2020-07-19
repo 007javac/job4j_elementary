69a9a7500bfa4e8756c39bcf7f74ff253b878c34
007javac <yurakaramyshev@yandex.com>
2020-07-19 23:05:50 +0200

    5.0.1. Сумма чисел. [#226416]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CounterSum.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CounterSum {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        int index = 0;
+        for (index = start; index <= finish; index++) {
+            sum = sum + index;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(0, 4));
+        System.out.println(sum(3, 6));
+        System.out.println(sum(5, 8));
+    }
+}
