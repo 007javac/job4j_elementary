0b4cc209525fd4eba7d288515948fff0f99f0bec
Iurii Karamyshev <yurakaramyshev@yandex.com>
2022-05-03 18:24:16 +0200

    Статические и не статические методы (Исправлено)

--- chapter_001/src/main/java/ru/job4j/oop/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/oop/Calculator.java
@@ -16,20 +16,20 @@ public class Calculator {
         return x * a;
     }
 
-    public int devide(int a) {
+    public int divide(int a) {
         return a / x;
     }
 
     public int sumAllOperation(int a) {
-        return sum(a) + minus(a) + multiply(a) + devide(a);
+        return sum(a) + minus(a) + multiply(a) + divide(a);
     }
     public static void main(String[] args) {
         int sum = sum(5);
         int minus = minus(5);
         Calculator calculator = new Calculator();
         int multiply = calculator.multiply(5);
-        int devide = calculator.devide(5);
-        int result = sum + minus + multiply + devide;
+        int divide = calculator.divide(5);
+        int result = sum + minus + multiply + divide;
         System.out.println("Сумма операций: " + result);
         result = calculator.sumAllOperation(5);
         System.out.println("Через метод sumAllOperation : " + result);
