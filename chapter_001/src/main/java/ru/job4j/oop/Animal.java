73c28a211ea8524d6dbe6911cce2334312647a28
Iurii Karamyshev <yurakaramyshev@yandex.com>
2022-06-10 23:03:17 +0200

    проверка

--- chapter_001/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_001/src/main/java/ru/job4j/oop/Animal.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 /**
  * Animal наследует класс Predator.
  * @author Karamyshev Iurii (007it@libero.it).
- * @since 19.05.2020.
+ * @since 19.05.2021.
  * @version 1
  */
 public class Animal extends Predator {
